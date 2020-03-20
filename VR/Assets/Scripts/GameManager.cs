89cd8885033737a40572257c540db1111fad952f
KID <50666207+KID421@users.noreply.github.com>
2020-03-20 11:49:42 +0800

    設定 Google VR 新增 C#

---
+++ VR/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // 定義欄位 (宣告變數)
+    // 修飾詞 類型 名稱;
+    // public 公開：顯示在屬性面板上
+    // GameObject 遊戲物件：儲存階層面板內的物件
+    
+    public GameObject groupLight;
+}
