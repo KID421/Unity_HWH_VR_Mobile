d4706d831390cc3cbc5074eb6b26b1ab8ee6a4d4
KID <50666207+KID421@users.noreply.github.com>
2020-03-27 09:53:39 +0800

    完成燈光閃爍效果

--- VR/Assets/Scripts/GameManager.cs
+++ VR/Assets/Scripts/GameManager.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using System.Collections;   // 引用 系統.集合 API：延遲 - 微軟 API
 
 public class GameManager : MonoBehaviour
 {
@@ -15,19 +16,31 @@ public class GameManager : MonoBehaviour
     // 定義方法 (Method)：有特定內容的功能
     // 修飾詞 傳回類型 方法名稱 () { 敘述 }
     // void 無傳回：使用此方法不會得到任何資料
+    // IEnumerator 延遲傳回
+    // 協同程序：多線程城市處理方式
     /// <summary>
     /// 燈光閃爍效果
     /// </summary>
-    public void LightEffect()
+    public IEnumerator LightEffect()
     {
         // 燈光群組.啟動設定(隱藏)
         groupLight.SetActive(false);
+        yield return new WaitForSeconds(0.2f);
+        groupLight.SetActive(true);
+        yield return new WaitForSeconds(0.3f);
+        groupLight.SetActive(false);
+        yield return new WaitForSeconds(0.1f);
+        groupLight.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        groupLight.SetActive(false);
+        yield return new WaitForSeconds(0.15f);
+        groupLight.SetActive(true);
     }
 
     // 事件：開始 - 播放時執行一次，初始化或遊戲開始需要的內容
     private void Start()
     {
-        // 呼叫自定義方法
-        LightEffect();
+        //LightEffect();                        // 呼叫自定義方法：一般呼叫方式
+        StartCoroutine(LightEffect());          // 呼叫協程方式
     }
 }
