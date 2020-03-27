0752028bd24aacda7da2d6ac01023106384db36f
KID <50666207+KID421@users.noreply.github.com>
2020-03-27 10:35:39 +0800

    控制寶箱朝自己前方移動

--- VR/Assets/Scripts/GameManager.cs
+++ VR/Assets/Scripts/GameManager.cs
@@ -12,12 +12,14 @@ public class GameManager : MonoBehaviour
     /// </summary>
     [Header("燈光群組")]
     public GameObject groupLight;
+    [Header("會動的寶箱")]
+    public Transform chest;
 
     // 定義方法 (Method)：有特定內容的功能
     // 修飾詞 傳回類型 方法名稱 () { 敘述 }
     // void 無傳回：使用此方法不會得到任何資料
     // IEnumerator 延遲傳回
-    // 協同程序：多線程城市處理方式
+    // 協同程序：多線程程式處理方式
     /// <summary>
     /// 燈光閃爍效果
     /// </summary>
@@ -35,6 +37,15 @@ public class GameManager : MonoBehaviour
         groupLight.SetActive(false);
         yield return new WaitForSeconds(0.15f);
         groupLight.SetActive(true);
+    }
+
+    public IEnumerator MoveChest()
+    {
+        // 前：forward
+        // 右：right
+        // 上：up
+        chest.position -= chest.forward;            // 寶箱.座標 遞減 寶箱.前方
+        yield return new WaitForSeconds(0.01f);
     }
 
     // 事件：開始 - 播放時執行一次，初始化或遊戲開始需要的內容
